473bcb517ed0758610246e4751c7e5a8c8a3cdfc
Gustavo Paulo Medeiros da Silva <gustavomedeirosdasilva@gmail.com>
2020-09-14 20:00:06 -0300

    fake i2c class inherits from smbus2

--- implementacao/fake_devices/fake_interfaces/fake_i2c.py
+++ implementacao/fake_devices/fake_interfaces/fake_i2c.py
@@ -3,9 +3,11 @@
 # References:
 #  Article "Using the I2C-Stub to Emulate a Device"
 #   (https://eraretuya.github.io/2016/12/10/the-i2c-stub-in-action/)
+#  smbus2 (https://github.com/kplindegaard/smbus2/)
 
 import os
 import subprocess
+import smbus2
 
 class FakeI2cException(Exception):
     """
@@ -13,25 +15,45 @@ class FakeI2cException(Exception):
     """
     pass
 
-class FakeI2c:
+
+class FakeI2cBus(smbus2.SMBus):
     """
     Fake I2C bus implementation.
     """
-    def __init__(self, bus):
+    def __init__(self, bus=None, devices_list=None):
         """
-        Load i2c-dev module.
+        Initialize and (optionally) open an i2c bus connection.
         """
+        super().__init__()
         self.__bus = bus
-        self.__real_bus = -1
+        self.__real_bus = None
         self.__bus_link = None
         self.__real_bus_path = None
-        self.__run_cmd_raise_exception('modprobe i2c-dev')
+        self.__devices_list = None
+        if bus is not None:
+            self.open(bus, devices_list)
 
     def __del__(self):
-        self.del_fake_devices()
+        self.close()
 
     def __str__(self):
         return self.bus_path()
+
+    def open(self, bus, devices_list):
+        """
+        Open a given i2c bus.
+        """
+        self.__bus = bus
+        self.__devices_list = devices_list
+        self.__add_fake_devices(devices_list)
+        super().open(self.__real_bus)
+
+    def close(self):
+        """
+        Close the i2c connection.
+        """
+        super().close()
+        self.__del_fake_devices()
 
     def bus(self):
         """
@@ -40,6 +62,9 @@ class FakeI2c:
         return self.__bus
 
     def bus_path(self):
+        """
+        Get the path of the fake i2c bus (symlink).
+        """
         if self.__bus_link:
             return self.__bus_link
         return ''
@@ -51,26 +76,31 @@ class FakeI2c:
         return self.__real_bus
 
     def real_bus_path(self):
+        """
+        Get the path of the real i2c bus (symlink).
+        """
         if self.__real_bus_path:
             return self.__real_bus_path
         return ''
 
-    def add_fake_devices(self, devices_list):
+    def __add_fake_devices(self, devices_list):
         """
         Load i2c-stub with fake devices.
         """
+        self.__run_cmd_raise_exception('modprobe i2c-dev')
+        self.__run_cmd_raise_exception('modprobe -r i2c-stub')
         list_addr = ','.join(map(hex, devices_list))
         cmd = 'modprobe i2c-stub chip_addr=' + list_addr
         output = self.__run_cmd_raise_exception(cmd)
         self.__symlink_i2c_stub()
 
-    def del_fake_devices(self):
+    def __del_fake_devices(self):
         """
         Unload i2c-stub.
         """
         try:
-            os.unlink(self.__bus_link)
             self.__run_cmd('modprobe -r i2c-stub')
+            os.unlink(self.__bus_link)
         except:
             # ignore errors
             pass
--- implementacao/tests/fake_devices/fake_interfaces/fake_i2c.py
+++ implementacao/tests/fake_devices/fake_interfaces/fake_i2c.py
@@ -7,20 +7,27 @@ from fake_devices.fake_interfaces import fake_i2c
 
 fake_i2c_bus = 101
 fake_i2c_devices = [0x05, 0x16, 0x27]
-fake_i2c = fake_i2c.FakeI2c(fake_i2c_bus);
-
-import time
+fake_i2c = fake_i2c.FakeI2cBus()
+data = [0x03, 0x04, 0x05]
 
 class TestFakeI2cBus(unittest.TestCase):
-    def test_0_load_modules(self):
-        fake_i2c.add_fake_devices(fake_i2c_devices)
+    def test_0_open(self):
+        fake_i2c.open(fake_i2c_bus, fake_i2c_devices)
         path_exists = os.path.exists(os.readlink(fake_i2c.bus_path()))
         self.assertTrue(path_exists, 'load i2c module failed')
 
-    def test_1_unload_modules(self):
+    def test_1_write(self):
+        fake_i2c.write_i2c_block_data(fake_i2c_devices[0], 0x00, data)
+
+    def test_2_read(self):
+        read_data = fake_i2c.read_i2c_block_data(fake_i2c_devices[0], 0x00,
+                len(data))
+        self.assertEqual(data, read_data, 'read failed')
+
+    def test_3_close(self):
         bus_path = fake_i2c.real_bus_path()
         bus_path_link = fake_i2c.bus_path()
-        fake_i2c.del_fake_devices()
+        fake_i2c.close()
         path_exists = os.path.exists(bus_path_link)
         self.assertFalse(path_exists, 'unload i2c module failed: symlink')
         path_exists = os.path.exists(bus_path)
