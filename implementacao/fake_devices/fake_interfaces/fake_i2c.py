d2509eb13f70c9b189128f4865dc12706ced2036
Gustavo Paulo Medeiros da Silva <gustavomedeirosdasilva@gmail.com>
2020-09-14 14:07:45 -0300

    add try except in fake i2c class

--- implementacao/fake_devices/fake_interfaces/fake_i2c.py
+++ implementacao/fake_devices/fake_interfaces/fake_i2c.py
@@ -3,39 +3,32 @@
 # References:
 #  Article "Using the I2C-Stub to Emulate a Device"
 #   (https://eraretuya.github.io/2016/12/10/the-i2c-stub-in-action/)
-#  smbus2 (https://github.com/kplindegaard/smbus2)
-
-
-# $ modprobe i2c-dev
-# $ modprobe i2c-stub chip_addr=0x03
-# $ i2cdetect -l
-# ...
-# i2c-8 smbus   SMBus stub driver   SMBus adapter
-# ...
-# $ i2cdetect -y 8
-# $ modprobe -r i2c-stub # Remove module
 
 import os
 import subprocess
 
-#import smbus
-#import smbus2 as smbus
-import smbus2
+class FakeI2cException(Exception):
+    """
+    Base class for fake i2c bus related exceptions.
+    """
+    pass
 
 class FakeI2c:
     """
     Fake I2C bus implementation.
     """
     def __init__(self, bus):
-        self.__run_cmd('modprobe i2c-dev')
+        """
+        Load i2c-dev module.
+        """
         self.__bus = bus
         self.__real_bus = -1
-
+        self.__bus_link = None
+        output = self.__run_cmd_raise_exception('modprobe i2c-dev')
     def __del__(self):
         try:
             os.unlink(self.__bus_link)
-            cmd = 'modprobe -r i2c-stub'
-            self.__run_cmd(cmd)
+            self.__run_cmd('modprobe -r i2c-stub')
         except:
             # ignore errors
             pass
@@ -46,37 +39,66 @@ class FakeI2c:
         return ''
 
     def bus(self):
+        """
+        Get the number of the fake i2c bus (symlink).
+        """
         return self.__bus
 
     def real_bus(self):
+        """
+        Get the number of the real i2c bus.
+        """
         return self.__real_bus
 
     def add_fake_devices(self, devices_list):
+        """
+        Load i2c-stub with fake devices.
+        """
         list_addr = ','.join(map(hex, devices_list))
         cmd = 'modprobe i2c-stub chip_addr=' + list_addr
-        self.__run_cmd(cmd)
+        output = self.__run_cmd_raise_exception(cmd)
         self.__symlink_i2c_stub()
 
     def __run_cmd(self, cmd):
+        """
+        Run shell command.
+        """
         result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
         return result.returncode, result.stdout.decode(), result.stderr.decode()
 
+    def __run_cmd_raise_exception(self, cmd):
+        """
+        Run shell command and raise an exception when an error occurs.
+        """
+        result = self.__run_cmd(cmd)
+        if result[0] != 0:
+            raise FakeI2cException('Cannot run command \'{}\'. '\
+                    'Shell returns: [{}] - {}'\
+                    .format(cmd, result[0], result[2]))
+        return result[0], result[1]
+
     def __symlink_i2c_stub(self):
-        output = self.__run_cmd('i2cdetect -l')
-        i2c_bus = ''
+        """
+        Identify the bus i2c-stub and create a symlink.
+        """
+        output = self.__run_cmd_raise_exception('i2cdetect -l')
+        i2c_bus = None
         for line in output[1].split('\n'):
             if 'SMBus stub driver' in line:
                 i2c_bus = line.split()[0]
                 self.__real_bus = int(i2c_bus.strip('i2c-'))
 
+        if i2c_bus is None:
+            raise FakeI2cException('SMBus stub driver not found')
+
         self.__bus_link = '/dev/i2c-' + str(self.__bus)
-        os.symlink('/dev/' + i2c_bus, self.__bus_link)
+        try:
+            if (os.path.islink(self.__bus_link) and not
+                    os.path.exists(os.readlink(self.__bus_link))):
+                os.unlink(self.__bus_link)
 
-
-fake_i2c_bus = FakeI2c(113)
-fake_i2c_bus.add_fake_devices([0x11, 0x12, 0x15])
-
-import time
-while True:
-    time.sleep(1)
+            if not os.path.islink(self.__bus_link):
+                os.symlink('/dev/' + i2c_bus, self.__bus_link)
+        except Exception as e:
+            raise FakeI2cException('Cannot create fake i2c symlink: ' + repr(e))
